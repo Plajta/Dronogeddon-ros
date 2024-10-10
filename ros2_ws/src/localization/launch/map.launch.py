6d1779477c3896f0673b99bbb1e29639ebadb5a4
Apolo-11 <michal.rejda@centrum.cz>
2024-10-10 17:19:39 +0200

    inicialni commit pro localization

---
+++ ros2_ws/src/localization/launch/map.launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='rtabmap_ros',
+            executable='rtabmap',
+            name='rtabmap',
+            output='screen',
+        ),
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=['config/ekf.yaml']
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', 'rviz/rviz_config.rviz']
+        )
+    ])
